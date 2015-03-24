7b1cf5e500915c1ecfb5c414562178e7336863b8
Kenny <chenkenn@gmail.com>
2015-03-24 12:56:55 -0600

    adding web guesser

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,24 +2,31 @@ require 'sinatra'
 require 'sinatra/reloader'
 SECRET_NUMBER = rand(100)
 
+@@guess_count = 5
+
 def check_guess(guess)
-  case
-  when guess.to_i == SECRET_NUMBER
-    "You got it right!"
-  when guess.to_i > SECRET_NUMBER + 5
-    "Way Too High"
-  when guess.to_i < SECRET_NUMBER - 5
-    "Way Too Low"
-  when guess.to_i > SECRET_NUMBER
-    "Too High"
-  else guess.to_i < SECRET_NUMBER
-    "Too Low"
-  end
+    @@guess_count - 1
+    case
+    # when @@guess_count == 0
+    #   @guess_count = 5
+    when guess.to_i == SECRET_NUMBER
+      "You got it right!"
+    when guess.to_i > SECRET_NUMBER + 5
+      "Way Too High"
+    when guess.to_i < SECRET_NUMBER - 5
+      "Way Too Low"
+    when guess.to_i > SECRET_NUMBER
+      "Too High"
+    else guess.to_i < SECRET_NUMBER
+      "Too Low"
+    end
 end
 
 
 get '/' do
   guess = params["guess"]
+  cheat = params["cheat"]
   message = check_guess(guess)
-  erb :index, :locals => {:guess => guess.to_i, :number => SECRET_NUMBER, :message => message}
+
+  erb :index, :locals => {:guess => guess.to_i, :number => SECRET_NUMBER, :message => message, :cheat => cheat}
 end
